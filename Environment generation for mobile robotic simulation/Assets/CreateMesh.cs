d5ef05cf855fc1f3359f5e9c476f3efae9ed3346
D0lan1 <martin.klemets@gmail.com>
2022-01-24 01:16:23 +0100

    Added first iteration

---
+++ Environment generation for mobile robotic simulation/Assets/CreateMesh.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class CreateMesh : MonoBehaviour
+{
+    Mesh mesh;
+    Vector3[] vertices;
+    int[] triangles;
+
+    Color[] color;
+    
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        CreateShape();
+        UpdateMesh();
+    }
+
+    void CreateShape (){
+        vertices = new Vector3[]{
+            new Vector3 (0,0,0),
+            new Vector3 (0,0,1),
+            new Vector3 (1,0,0),
+            new Vector3 (1,0,1)
+        };
+
+        triangles = new int[]{
+            0,1,2,
+            1,3,2
+        };
+
+
+    }
+    void UpdateMesh(){
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+}
